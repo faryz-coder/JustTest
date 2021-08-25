155f26026087ac8916eac3963978fdfc2a1d6988
faryz-coder <faryz.coder@gmail.com>
2021-08-25 15:14:22 +0800

    remove extra comma at line 20 and 99

--- app/src/main/java/com/faryz/testapp/first/FirstFragment.kt
+++ app/src/main/java/com/faryz/testapp/first/FirstFragment.kt
@@ -5,7 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewTreeLifecycleOwner
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.faryz.testapp.R
@@ -36,6 +38,10 @@ class FirstFragment : Fragment() {
             layoutManager = LinearLayoutManager(this@FirstFragment.context)
             adapter = UserList(userList)
         }
+
+        userListViewModel.users.observe(viewLifecycleOwner, Observer {
+
+        })
 
 
 
