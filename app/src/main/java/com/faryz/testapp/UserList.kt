3025b13d8824354fa595e54dda267dae0c921cd7
faryz-coder <faryz.coder@gmail.com>
2021-08-25 14:53:18 +0800

    create recyclerView for user list layout

--- app/src/main/java/com/faryz/testapp/UserList.kt
+++ app/src/main/java/com/faryz/testapp/UserList.kt
@@ -3,12 +3,15 @@ package com.faryz.testapp
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.RecyclerView
 
 class UserList(private val userList: MutableList<ListUser>) :
     RecyclerView.Adapter<UserList.ViewHolder>() {
     class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
-
+        val name: TextView = itemView.findViewById(R.id.user_list_name)
+        val row: ConstraintLayout = itemView.findViewById(R.id.user_list_row)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -17,7 +20,8 @@ class UserList(private val userList: MutableList<ListUser>) :
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        val loc = userList[position]
+        holder.name.text = "${loc.firstName} ${loc.lastName}"
     }
 
     override fun getItemCount(): Int {
